62ee3913c5c2936a17bec8466a50a54f34c713a1
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-04-24 11:56:48 -0400

    MVP structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
+import React, { useState } from 'react';
 import Navbar from './components/navbar/Navbar';
 import Display from './components/display/Display';
+import style from './App.css';
 
-function App() {
+function App(props) {
+
+  const [isMarket, setIsMarket] = useState(true);
+  const [isMyStore, setIsMyStore] = useState(false);
+
+  const [isShoppingCart, setIsShoppingCart] = useState(false);
+  const [isAuth, setIsAuth] = useState(false);
+
   return (
-    <div>
-      <Navbar />
-      <Display />
-      <ModalDisplay />
-      {/* This is a test by Reed and should only be in the reed branch */}
+    <div className="App">
+      <Navbar 
+        setIsMarket = {setIsMarket}
+        setIsMyStore = {setIsMyStore}
+        setIsAuth = {setIsAuth}
+        setIsShoppingCart = {setIsShoppingCart}/>
+      <Display 
+        isMarket = {isMarket} 
+        isMyStore = {isMyStore} />
     </div>
   );
 }
