98a2188289c826440a559e029a84c3e17029c644
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-04-25 14:50:14 -0400

    working product.js

--- src/components/maincomponents/Product.js
+++ src/components/maincomponents/Product.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 function Product(props) {
-    return(
+
+    const {productName, price, description, stock, storeOwner, imageURL} = props;
+
+    return (
         <div>
-            This is the Product
+            <Card>
+                <CardImg className = "cardImage" top width="100%" src={imageURL} alt="Card image cap" />
+                <CardBody>
+                    <CardTitle tag="h5">{productName}</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{price}</CardSubtitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{stock}</CardSubtitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">{storeOwner}</CardSubtitle>
+                    <CardText>{description}</CardText>
+                    <Button>Add to Cart</Button>
+                </CardBody>
+            </Card>
         </div>
     );
 }
