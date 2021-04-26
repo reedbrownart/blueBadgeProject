0896b28c0be389a5c0b47e87742125d823324fad
DeneaRansom <webb.denea@gmail.com>
2021-04-26 09:10:56 -0400

    pulling from Develop/Auth and Shopping Cart

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Display from './components/display/Display';
 
+
 function App() {
   return (
     <div>
+    
       <Navbar />
       <Display />
-      <ModalDisplay />
+      
     </div>
   );
 }
 
 export default App;
 
-//Testing-Denea
--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -10,7 +10,7 @@ function Display() {
             <Auth />
             <Market />
             <MyStore />
-            <ShoppingCart />
+          
         </div>
     );
 }
