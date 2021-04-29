9d542db6192a9d9611ba91461d3e653ce1b56bd1
reedbrownart <reedb@DESKTOP-COU5FD4>
2021-04-29 09:17:22 -0400

    our first deployment to heroku for the client side

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3500';
         break;
-    case 'kcp-pieclient.herokuapp.com':
+    case 'blue-badge-agora-server.herokuapp.com':
         APIURL = 'https://blue-badge-agora-server.herokuapp.com'
 }
 
